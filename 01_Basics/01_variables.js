227701d229f2fa0e4c834498827b5a6507eb157d
jayeshbadgujar678 <98027048+jayeshbadgujar678@users.noreply.github.com>
2023-08-01 09:05:14 +0000

    variables Basics

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const AccountID = 231432
+let AccountPassword = "1234"
+var AccountEmail = "jayesh@bitblocks.in"
+
+AccountCity = "Newyork"
+AccountState = "Penselvenia,US"
+
+/* Prefer using let rather than var it's deprecated */
+
+console.table([AccountID,AccountPassword,AccountEmail, AccountCity, AccountState])
