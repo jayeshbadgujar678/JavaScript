a131fc944e8cca7515ae866cbabcb0bf5c0ce1a9
jayeshbadgujar678 <98027048+jayeshbadgujar678@users.noreply.github.com>
2023-08-02 02:10:04 +0000

    Datatype

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all JS code as newer version
+
+// alert( 3+ 3) // We are using node.js, not browser
+
+let Name = "Jayesh" // String DT
+let age = 18 // Number DT
+let isLoggedIn = false //Boolean DT
+let state = null // Null DT
+
+// number => 2 ^ 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
